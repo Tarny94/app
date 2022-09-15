b953f82ad3c73cee3a08872b4569b167b3be8c4c
Tarny94 <102902000+Tarny94@users.noreply.github.com>
2022-09-15 20:16:02 +0300

    create login page and avoid useless data ffrom app

---
+++ src/Pages/Login.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "../style/login.scss";
+
+const Login = () => {
+  return (
+    <div className="login-container">
+      Sign in
+      <div>
+        <input></input>
+      </div>
+      <div>
+        <input></input>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
