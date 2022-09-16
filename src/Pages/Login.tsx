53a33bc63e2abcf76494e4380753cfdd75e7dfbd
Tarny94 <102902000+Tarny94@users.noreply.github.com>
2022-09-16 22:01:51 +0300

    style login page

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -4,12 +4,33 @@ import "../style/login.scss";
 const Login = () => {
   return (
     <div className="login-container">
-      Sign in
-      <div>
-        <input></input>
-      </div>
-      <div>
-        <input></input>
+      <div className="input-container">
+        <div className="box-auth">
+          <h1 className="login-title">Sign in</h1>
+          <div className="input-field">
+            <input
+              className="input-login input-auth "
+              placeholder="EMAIL or ID"
+              type="text"
+            ></input>
+          </div>
+          <div className="input-field">
+            <input className="input-login" placeholder="PASSWORD"></input>
+          </div>
+          <div>
+            <div>...</div>
+          </div>
+          <div className="password-word">
+            <p>I forgot the password</p>
+          </div>
+          <div>
+            <button className="authenticate-button">Authenticate</button>
+          </div>
+          <div className="account-create-field">
+            <p>Do not have an account yet?</p>
+            <p>Create a new one</p>
+          </div>
+        </div>
       </div>
     </div>
   );
