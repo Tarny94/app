6f99f10e6aff122574fd362a9b2d2d5245dc7b9d
Tarny94 <102902000+Tarny94@users.noreply.github.com>
2022-11-08 19:28:00 +0200

    Refactor design from Login and Registration components

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -5,31 +5,29 @@ const Login = () => {
   return (
     <div className="login-container">
       <div className="input-container">
-        <div className="box-auth">
-          <h1 className="login-title">Sign in</h1>
-          <div className="input-field">
-            <input
-              className="input-login input-auth "
-              placeholder="EMAIL or ID"
-              type="text"
-            ></input>
-          </div>
-          <div className="input-field">
-            <input className="input-login" placeholder="PASSWORD"></input>
-          </div>
-          <div>
-            <div className="error-message"></div>
-          </div>
-          <div className="password-word">
-            <p>I forgot the password</p>
-          </div>
-          <div>
-            <button className="authenticate-button">Authenticate</button>
-          </div>
-          <div className="account-create-field">
-            <p>Do not have an account yet?</p>
-            <p>Create a new one</p>
-          </div>
+        <h1 className="login-title">Sign in</h1>
+        <div className="input-field">
+          <input
+            className="input-login input-auth "
+            placeholder="EMAIL or ID"
+            type="text"
+          ></input>
+        </div>
+        <div className="input-field">
+          <input className="input-login" placeholder="PASSWORD"></input>
+        </div>
+        <div>
+          <div className="error-message"></div>
+        </div>
+        <div className="password-word">
+          <p>I forgot the password</p>
+        </div>
+        <div>
+          <button className="authenticate-button">Authenticate</button>
+        </div>
+        <div className="account-create-field">
+          <p>Do not have an account yet?</p>
+          <p>Create a new one</p>
         </div>
       </div>
     </div>
